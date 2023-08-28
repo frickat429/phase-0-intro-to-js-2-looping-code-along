a174b1ca8e01632567f502edec9fba96f63c1a6c
Austin Frick <austin.frick429@gmail.com>
2023-08-28 17:12:07 -0400

    completeloop

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+
+
+
+
+
+
+    function writeCards(names, event,) { 
+        const message = []
+        for (let i = 0; i < names.length; i++) { 
+            message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+            debugger
+        
+      } 
+      return message
+}
+
+
+
+function countDown(i) {
+while (i >= 0 ) {
+  console.log(i); 
+  i--
+}
+} 
